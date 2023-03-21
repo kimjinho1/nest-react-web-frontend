407c9207499f7c21bb551cdc23a8960fd58b8c68
kimjinho1 <rlawlsgh8113@naver.com>
2023-03-21 23:44:51 +0900

    FEAT: useEffect

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import { useState, useEffect } from "react";
+import UseEffect from "./UseEffect";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => console.log("Run only one"), []);
-  useEffect(() => console.log("run when counter changes"), [counter]);
-  useEffect(() => console.log("run when keyword changes"), [keyword]);
-  useEffect(
-    () => console.log("run when keyword & counter changes"),
-    [counter, keyword]
-  );
-  return (
-    <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
-    </div>
-  );
+  return <UseEffect />;
 }
 
 export default App;
