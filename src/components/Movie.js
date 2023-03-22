e9a9ef92ad06f63a1c5e7b112d0f0045035b14dd
kimjinho1 <rlawlsgh8113@naver.com>
2023-03-22 20:08:47 +0900

    FEAT: first router

---
+++ src/components/Movie.js
@@ -0,0 +1,30 @@
+import { PropTypes } from "prop-types";
+import { Link } from "react-router-dom";
+
+function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <div>
+        <img src={coverImg} alt={title} />
+        <h2>
+          <Link to="/movie">{title}</Link>
+        </h2>
+        <p>{summary}</p>
+        <ul>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
--- src/study/ToDo1.js
+++ src/study/ToDo1.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function ToDo1() {
   const [toDo, setToDo] = useState("");
