0950edb5770885116c8106a923eee60d432c035b
kimjinho1 <rlawlsgh8113@naver.com>
2023-03-23 08:20:06 +0900

    Create Navvar, Home, ToDO, Auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "./routes/Home";
-import Detail from "./routes/Detail";
+import Home from "./Home/Home";
+import Navbar from "./Navbar/Navbar";
+import ToDo from "./ToDo/ToDo";
+import Authentication from "./Authentication/Authentication";
+
+const USER_TOKEN = "USER_TOKEN";
 
 function App() {
+  const [loginState, setLoginState] = useState(false);
+  // 로그인 되어 있는 상태인지 확인
+  useEffect(() => {
+    if (localStorage.getItem(USER_TOKEN)) {
+      console.log("Login O");
+      setLoginState(true);
+    } else {
+      console.log("Login X");
+    }
+  }, []);
+
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/movie" element={<Detail />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home loginState={loginState} />} />
+        <Route path="/todo" element={<ToDo loginState={loginState} />} />
+        <Route
+          path="/auth"
+          element={
+            <Authentication
+              loginState={loginState}
+              setLoginState={setLoginState}
+            />
+          }
+        />
       </Routes>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const API_ID = process.env.REACT_APP_GOOGLE_OAUTH_API_ID;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <GoogleOAuthProvider clientId={`${API_ID}`}>
+    <App />
+  </GoogleOAuthProvider>
 );
