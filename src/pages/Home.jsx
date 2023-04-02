667de0cc33e536b31cdd57db68a9728b4cf08517
kimjinho1 <rlawlsgh8113@naver.com>
2023-04-03 06:52:56 +0900

    CHORE: Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = ({ loginState }) => {
       ) : (
         <h1>로그인한 상태입니다</h1>
       )}
-      <h1>React 생각보다 재미있다</h1>
+      <h1>트센 학습용 웹사이트</h1>
       <img src="logo192.png" alt="logo" />
     </HomeDiv>
   );
