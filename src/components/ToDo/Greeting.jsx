bcaa6d0476529065478e068a7f8e5daef85cc5dc
kimjinho1 <rlawlsgh8113@naver.com>
2023-04-03 04:34:34 +0900

    FEAT: Greeting todo

--- src/components/ToDo/Greeting.jsx
+++ src/components/ToDo/Greeting.jsx
@@ -10,21 +10,21 @@ const GreetingDiv = styled.div`
   }
 `;
 
-const Greeting = ({loginState, toDos}) => {
-    return (
-        loginState === false ? (
-          <GreetingDiv><h1>로그인하고 오세요</h1></GreetingDiv>
-        ) : (
-          <GreetingDiv>
-            <h1>어서오세요!</h1>
-            {toDos.length === 0 ? (
-                <h1>할 일이 없으시네요? 그럴리가 없는데?</h1>
-            ) : (
-                <h1>할 일이 {toDos.length}개 남았어요</h1>
-            )}
-          </GreetingDiv>
-        )
-    );
-}
+const Greeting = ({ loginState, toDos }) => {
+  return loginState === false ? (
+    <GreetingDiv>
+      <h1>로그인하고 오세요</h1>
+    </GreetingDiv>
+  ) : (
+    <GreetingDiv>
+      <h1>어서오세요!</h1>
+      {toDos.length === 0 ? (
+        <h1>할 일이 없다!</h1>
+      ) : (
+        <h1>할 일이 {toDos.length}개 남았어요</h1>
+      )}
+    </GreetingDiv>
+  );
+};
 
 export default Greeting;
