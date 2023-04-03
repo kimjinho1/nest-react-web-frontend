0748128c2546d817216be2193276b292e18f401c
kimjinho1 <rlawlsgh8113@naver.com>
2023-04-03 09:56:59 +0900

    FEAT: 게시판 목록 페이지 거의 완성

--- src/components/boards/BoardList.jsx
+++ src/components/boards/BoardList.jsx
@@ -2,18 +2,54 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import styled from "styled-components";
 
-const BoardListUl = styled.ul`
-  margin: 0;
-  padding: 0;
-  list-style-type: none;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+const PostListContainer = styled.div`
+  padding: 15px;
+`;
 
-  li {
-    padding-top: 8px;
-    font-size: 25px;
+const PostCard = styled.div`
+  display: flex;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3);
+  &:hover {
+    transform: scale(1.04);
   }
+`;
+
+const PostThumbnail = styled.img`
+  width: 150px;
+  height: 100px;
+  object-fit: cover;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+`;
+
+const PostContentWrapper = styled.div`
+  padding: 20px;
+`;
+
+const PostTitle = styled.h2`
+  margin: 0;
+  font-size: 24px;
+  font-weight: bold;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const PostMeta = styled.div`
+  margin-top: 5px;
+  font-size: 14px;
+`;
+
+const PostNickname = styled.div`
+  font-weight: bold;
+  margin-right: 10px;
+`;
+
+const PostDate = styled.div`
+  color: #777;
 `;
 
 const BoardList = ({ loginState }) => {
@@ -43,15 +79,24 @@ const BoardList = ({ loginState }) => {
   }, []);
 
   return (
-    <BoardListUl>
-      {boards.map((board, index) => (
-        <li key={index}>
-          <span>{board.title}</span>
-          <span> | {board.userName}</span>
-          <span> | {board.content}</span>
-        </li>
+    <PostListContainer>
+      {boards.map((board) => (
+        <PostCard key={board.id}>
+          <PostThumbnail
+            src={
+              "https://velog.velcdn.com/images/cyheum/post/37534693-2ae7-4134-91ab-af7eb74b7efb/react-logo.png"
+            }
+          />
+          <PostContentWrapper>
+            <PostTitle>{board.title}</PostTitle>
+            <PostMeta>
+              <PostNickname>{board.userName}</PostNickname>
+              <PostDate>{"2023-04-03"}</PostDate>
+            </PostMeta>
+          </PostContentWrapper>
+        </PostCard>
       ))}
-    </BoardListUl>
+    </PostListContainer>
   );
 };
 
